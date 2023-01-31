a16880e92fd6de146ec1f56b8c8c5d4e27fbe581
Dimmazz <zaskok20@yandex.ru>
2023-01-31 13:39:24 +0300

    first commit

---
+++ data/projects.ts
@@ -0,0 +1,30 @@
+import RyanWarner from '../public/img/projects/ryan-warner.webp'
+import Polar from '../public/img/projects/polar.webp'
+import Marico from '../public/img/projects/marico.webp'
+
+
+const projects = [
+  {
+    img: RyanWarner,
+    category: 'Layout',
+    title: 'Ryan Warner',
+    viewLink: 'https://dimmazz.github.io/portfolio/Ryan.Warner/',
+    codeLink: 'https://github.com/Dimmazz/portfolio/tree/master/Ryan.Warner',
+  },
+  {
+    img: Polar,
+    category: 'Layout',
+    title: 'Polar',
+    viewLink: 'https://dimmazz.github.io/portfolio/polar-project/',
+    codeLink: 'https://github.com/Dimmazz/portfolio/tree/master/polar-project',
+  },
+  {
+    img: Marico,
+    category: 'PUG + Sass + Webpack app',
+    title: 'Marico',
+    viewLink: 'https://dimmazz.github.io/Marico-build/',
+    codeLink: 'https://github.com/Dimmazz/Marico-build',
+  }
+]
+
+export default projects
